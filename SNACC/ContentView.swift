0a77a0de3cdae3de5b29e1eebcc0edd1d7838a0b
Matthew Mehrtens <12023414+mcmehrtens@users.noreply.github.com>
2026-03-17 23:28:06 -0500

    Initial Commit

---
+++ SNACC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SNACC
+//
+//  Created by Matthew Mehrtens on 2026-03-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SNACC/SNACCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SNACCApp.swift
+//  SNACC
+//
+//  Created by Matthew Mehrtens on 2026-03-17.
+//
+
+import SwiftUI
+
+@main
+struct SNACCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
